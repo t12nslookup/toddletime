67e4dd5c4ac818d30a9aceb1f12289645f13162a
Jonathan Adams <t12nslookup@googlemail.com>
2016-02-17 11:24:58 +0000

    Gemfile update

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,6 @@
-AWS.config(access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-                        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] )
+#Aws.config({access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+#           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] })
 
-S3_BUCKET = AWS::S3.new.buckets[ENV['S3_BUCKET']]
+Aws.config.update({region: 'eu-west-1'})
+#S3_BUCKET = Aws::S3.new.buckets[ENV['S3_BUCKET']]
 
