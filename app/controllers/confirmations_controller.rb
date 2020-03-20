a74bf6e3cf0d9eeff0406a03ddb54bddf2c2cb07
Jonathan Adams <t12nslookup@googlemail.com>
2020-03-20 16:18:35 +0000

    skip_before_action issues

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,7 +1,7 @@
 class ConfirmationsController < Devise::ConfirmationsController
   # Remove the first skip_before_filter (:require_no_authentication) if you
   # don't want to enable logged users to access the confirmation page.
-  skip_before_action :require_no_authentication
+  skip_before_action :require_no_authentication, raise: false
   skip_before_action :authenticate_user!
 
   def index
